c574fef7a17f29349d4786e8a80b576a1958f4af
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-22 01:34:14 -0700

    Column display of 2 texts: message and from.

--- app/src/main/java/com/example/jonathan/testjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testjetpackcompose/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jonathan.testjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -10,6 +11,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jonathan.testjetpackcompose.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    BirthdayGreetingWithText("Happy Birthday Sam!")
+                    BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
                 }
             }
         }
@@ -30,16 +32,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun BirthdayGreetingWithText(message: String) {
-    Text(
-        text = message
-    )
+fun BirthdayGreetingWithText(message: String, from: String) {
+    Column {
+        Text(
+            text = message,
+            fontSize = 36.sp
+        )
+
+        Text(
+            text = from,
+            fontSize = 24.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("Happy Birthday Sam!")
+        BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
     }
 }
