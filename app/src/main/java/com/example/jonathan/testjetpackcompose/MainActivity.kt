22bb6cdd7522a904f84981846f125a51913e752e
Jonathan Zhong <33461789+jonathanzho@users.noreply.github.com>
2022-10-22 21:39:48 -0700

    Added a birthday Image to the UI, via Box, below the Text.

--- app/src/main/java/com/example/jonathan/testjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jonathan/testjetpackcompose/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.jonathan.testjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
@@ -10,6 +12,7 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import com.example.jonathan.testjetpackcompose.ui.theme.HappyBirthdayTheme
@@ -46,10 +49,24 @@ fun BirthdayGreetingWithText(message: String, from: String) {
     }
 }
 
+@Composable
+fun BirthdayGreetingWithImage(message: String, from: String) {
+    val image = painterResource(R.drawable.androidparty)
+
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+
+        BirthdayGreetingWithText(message, from)
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        BirthdayGreetingWithText("Happy Birthday Sam!", "- from Emma")
+        BirthdayGreetingWithImage("Happy Birthday Sam!", "- from Emma")
     }
 }
